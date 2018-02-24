0d68755b0e958b4d035b6b7f6b7f46bf0db5df0b
Bekzat Shayakhmetov <smbekzat@hotmail.com>
2018-02-24 18:04:49 +0700

    added icons, tables, and other aspects of the project

--- js/common.js
+++ js/common.js
@@ -1,3 +1,15 @@
-$(function() {
-    $( "#tabs" ).tabs();
+var dropdown = document.querySelectorAll('.dropdown-list'),
+    dropdownArray = Array.prototype.slice.call(dropdown, 0);
+
+dropdownArray.forEach(function(el) {
+    var button = el.querySelector('a[dropdown-prop="title"]'),
+        menu   = el.querySelector('.dropdown-list-items'),
+        icon   = button.querySelector('i.dropdown-icon'),
+
+        toggleDropdown = function() {
+            classie.toggleClass(menu, 'dropdown-list-open');
+            classie.toggleClass(icon, 'dropdown-icon-open');
+        };
+
+    button.addEventListener('click', toggleDropdown);
 });
